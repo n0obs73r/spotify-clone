83fe2f88e79310df4aa299839114e4001da3f1cb
n0obs73r <aryan.prince999@gmail.com>
2024-03-14 15:15:10 +0530

    progress bar updated

--- desi-spotify/src/app/components/player-controls.component.ts
+++ desi-spotify/src/app/components/player-controls.component.ts
@@ -1,34 +1,84 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from "@angular/core";
-import {PlayerService} from "../services/player.service";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { PlayerService } from "../services/player.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-player-controls',
   templateUrl: './player-controls.component.html',
-  styleUrls: ['./player-controls.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./player-controls.component.scss']
 })
-export class PlayerControlsComponent {
+export class PlayerControlsComponent implements OnInit, OnDestroy {
   currentProgress = 0;
+  startTime = "0:00";
+  endTime = "0:00";
+  progressSubscription: Subscription | null = null;
+  manualSeeking = false; // Flag to indicate manual seeking
 
-  constructor(
-    private cdr: ChangeDetectorRef,
-    public playerService: PlayerService
-  ) { }
+  constructor(public playerService: PlayerService) { }
 
-  handleProgressBarClick(e: MouseEvent) {
-    const target = e.target as HTMLElement;
-    if (target.classList.contains('thumb')) {
-      e.stopPropagation();
-      return;
+  ngOnInit() {
+    // Subscribe to progress updates
+    this.progressSubscription = this.playerService.getProgress().subscribe(progress => {
+      if (!this.manualSeeking) {
+        this.currentProgress = progress;
+        this.updateTime(progress);
+      }
+    });
+
+    // Subscribe to playing status changes
+    this.playerService.getPlaying().subscribe(playing => {
+      if (!playing) {
+        // Reset time and progress when playback stops
+        this.currentProgress = 0;
+        this.startTime = "0:00";
+        this.endTime = "0:00";
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe to prevent memory leaks
+    this.progressSubscription?.unsubscribe();
+  }
+
+  updateTime(progress: number) {
+    const totalDuration = this.playerService.getTotalDuration();
+    if (totalDuration) {
+      const currentTimeInSeconds = (progress / 100) * totalDuration;
+      this.startTime = this.formatTime(currentTimeInSeconds);
+      this.endTime = this.formatTime(totalDuration - currentTimeInSeconds); // Update endTime
     }
-    this.recalculateCurrentProgress(e.offsetX);
   }
 
-  recalculateCurrentProgress(offsetX: number) {
-    const progressBarWidth = 440;
-    this.currentProgress = (offsetX / progressBarWidth) * 100;
-    this.cdr.markForCheck();
+  formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
   }
+
+  handleProgressBarInput(event: Event) {
+    const value = (event.target as HTMLInputElement)?.value;
+    if (value !== null && value !== undefined) {
+      this.manualSeeking = true; // Set flag to indicate manual seeking
+      this.updateTime(Number(value)); 
+      this.currentProgress = Number(value); // Update progress bar position immediately
+      this.playerService.seekTo(Number(value)); // Seek to the selected position
+    }
+  }
+
+  handleProgressBarChange() {
+    if (!this.manualSeeking) {
+      // If manual seeking flag is not set, update the time and progress
+      const totalDuration = this.playerService.getTotalDuration();
+      if (totalDuration) {
+        const newPosition = (this.currentProgress / 100) * totalDuration;
+        this.updateTime(newPosition);
+        this.playerService.seekTo(newPosition);
+      }
+    }
+    this.manualSeeking = false; // Reset the manual seeking flag
+  }
+  
 
   togglePlayPause() {
     this.playerService.togglePlaying();
--- desi-spotify/src/app/services/player.service.ts
+++ desi-spotify/src/app/services/player.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Subject } from "rxjs";
 import { Song } from "../models/song.model";
-import { Howl } from 'howler';
+import { Howl, Howler } from 'howler';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,9 @@ export class PlayerService {
   private currentSongUrl: string | null = null;
   private playlist: Song[] = [];
   private currentIndex: number = 0;
+  private progress$ = new Subject<number>(); // Subject to emit playback progress
+  private duration: number = 0;
+  private progressInterval: any;
 
   constructor() {
     this.playing$.subscribe(playing => {
@@ -48,40 +51,64 @@ export class PlayerService {
   playSongService(song: Song) {
     const songUrl = this.getAudioUrl(song);
     console.log('Attempting to play song with URL:', songUrl);
-
-    // Check if a song is currently playing
-    if (this.currentHowl) {
-        // If a song is playing, stop it before playing the new song
-        this.stopCurrentSong();
+  
+    // Reset duration when loading a new song
+    this.duration = 0;
+  
+    // Emit an event indicating that a new song is loading
+    this.setPlaying(false);
+  
+    // Check if the song to play is the same as the currently playing song
+    if (this.currentSongUrl === songUrl && this.currentHowl) {
+      // If the song is the same and there's an existing Howl instance, just seek to the beginning
+      this.currentHowl.seek(0);
+      this.setPlaying(true); // Ensure the player is set to playing
+      return;
     }
-    
+  
+    // If a different song is requested or there's no existing Howl instance, create a new one
+    if (this.currentHowl) {
+      // If a song is playing and it's not the requested song, stop it before playing the new song
+      this.stopCurrentSong();
+    }
+  
     const howl = new Howl({
-        src: songUrl ? [songUrl] : [''],
-        onload: () => {
-            console.log('Audio file loaded successfully.');
-            console.log('Is current Howl loaded?', howl.state() === 'loaded');
-            this.setPlaying(true);
-            howl.play();
-            console.log('Song started playing.');
-        },
-        onloaderror: (id, error) => {
-            console.error('Error loading audio:', error);
-            this.stopCurrentSong();
-        },
-        onplayerror: (id, error) => {
-            console.error('Error playing audio:', error);
-            this.stopCurrentSong();
-        }
+      src: songUrl ? [songUrl] : [''],
+      onload: () => {
+        console.log('Audio file loaded successfully.');
+        console.log('Is current Howl loaded?', howl.state() === 'loaded');
+        this.setPlaying(true);
+        this.duration = howl.duration(); // Update duration when the song is loaded
+        console.log('Song started playing.');
+        // Start updating progress when the song starts playing
+        this.startProgressInterval();
+      },
+      onloaderror: (id, error) => {
+        console.error('Error loading audio:', error);
+        this.stopCurrentSong();
+      },
+      onplayerror: (id, error) => {
+        console.error('Error playing audio:', error);
+        this.stopCurrentSong();
+      },
+      onend: () => { // When the song ends, reset progress
+        this.progress$.next(0);
+        this.setPlaying(false); // Ensure the player is set to not playing when the song ends
+        this.resetProgressInterval();
+      },
+      onseek: (position) => { // When the song is seeked, update progress
+        this.progress$.next(position);
+      }
     });
-
+  
     console.log('Howl instance:', howl);
     console.log('Current Howl error:', howl.state() === 'unloaded' ? 'No error' : 'Error');
-
+  
     this.setCurrentHowl(howl);
     this.currentSongUrl = songUrl;
-}
-
-
+  }
+  
+  
 
   stopCurrentSong() {
     if (this.currentHowl) {
@@ -91,6 +118,7 @@ export class PlayerService {
       this.currentSongUrl = null;
       this.setPlaying(false);
       console.log('Song stopped.');
+      this.resetProgressInterval();
     }
   }
 
@@ -115,5 +143,36 @@ export class PlayerService {
   private loadCurrentSong() {
     const song = this.playlist[this.currentIndex];
     this.playSongService(song);
+  }
+
+  getProgress() {
+    return this.progress$.asObservable();
+  }
+
+  getTotalDuration() {
+    return this.duration;
+  }
+  
+  seekTo(position: number) {
+    if (this.currentHowl) {
+      this.currentHowl.seek(position);
+      if (!this.playing$.getValue()) {
+        // If the player is not playing, start the progress interval after seeking
+        this.startProgressInterval();
+      }
+    }
+  }
+  
+  
+
+  private startProgressInterval() {
+    this.progressInterval = setInterval(() => {
+      const seek = this.currentHowl?.seek() || 0;
+      this.progress$.next(seek);
+    }, 1000);
+  }
+
+  private resetProgressInterval() {
+    clearInterval(this.progressInterval);
   }
 }
